a3afbd8f0b4bb5fa94aba3fc194eb1e6d4c6dccf
ERHSQuizbowl <vinaichand@yahoo.com>
2019-08-10 20:12:44 -0400

    Fix port bug

--- app.js
+++ app.js
@@ -44,6 +44,6 @@ app.post('/addemail', (req, res) => {
     })
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on port');
 });
